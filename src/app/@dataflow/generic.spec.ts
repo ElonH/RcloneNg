a85200ed8767022204a10cc63b7bf9d4bc4855ab
ElonH <elonhhuang@gmail.com>
2020-05-13 20:58:06 +0800

    style: add pretter conf and format code

--- src/app/@dataflow/generic.spec.ts
+++ src/app/@dataflow/generic.spec.ts
@@ -3,31 +3,28 @@ import { Observable, of } from 'rxjs';
 import { AjaxResponse, AjaxRequest } from 'rxjs/ajax';
 
 class TestGeneric extends Generic {
-  cmd = 'test';
-  params = {};
-  cacheSupport = true;
-  protected prerequest(): Observable<DataFlowNode> {
-    return of([{ pre: 1 }, null]);
-  }
-  protected request(x: DataFlowNode): AjaxRequest {
-    return {} as AjaxResponse;
-  }
-  protected generateModule(
-    current: DataFlowNode,
-    previous: DataFlowNode
-  ): DataFlowNode {
-    return [{ pre: previous[0]['pre'], cur: 2 }, null];
-  }
+	cmd = 'test';
+	params = {};
+	cacheSupport = true;
+	protected prerequest(): Observable<DataFlowNode> {
+		return of([{ pre: 1 }, null]);
+	}
+	protected request(x: DataFlowNode): AjaxRequest {
+		return {} as AjaxResponse;
+	}
+	protected generateModule(current: DataFlowNode, previous: DataFlowNode): DataFlowNode {
+		return [{ pre: previous[0]['pre'], cur: 2 }, null];
+	}
 }
 
 describe('Generic', () => {
-  var g: TestGeneric;
-  beforeEach(() => {
-    g = new TestGeneric();
-    g.deploy();
-  });
+	var g: TestGeneric;
+	beforeEach(() => {
+		g = new TestGeneric();
+		g.deploy();
+	});
 
-  it('should create an instance', () => {
-    expect(g).toBeTruthy();
-  });
+	it('should create an instance', () => {
+		expect(g).toBeTruthy();
+	});
 });
--- src/app/@dataflow/generic.ts
+++ src/app/@dataflow/generic.ts
@@ -1,98 +1,88 @@
 import { Observable, of, iif } from 'rxjs';
 import {
-  switchMap,
-  map,
-  catchError,
-  tap,
-  withLatestFrom,
-  startWith,
-  distinctUntilChanged,
+	switchMap,
+	map,
+	catchError,
+	tap,
+	withLatestFrom,
+	startWith,
+	distinctUntilChanged,
 } from 'rxjs/operators';
 import { AjaxResponse, ajax, AjaxRequest } from 'rxjs/ajax';
 
 export type DataFlowNode = [object, any[]];
 
 export abstract class Generic {
-  protected abstract cmd: string;
-  protected abstract params: object;
-  protected abstract cacheSupport: boolean;
+	protected abstract cmd: string;
+	protected abstract params: object;
+	protected abstract cacheSupport: boolean;
 
-  protected cachePath?: string;
+	protected cachePath?: string;
 
-  protected abstract prerequest(): Observable<DataFlowNode>;
-  protected abstract request(x: DataFlowNode): AjaxRequest;
-  protected _request(x: DataFlowNode): Observable<AjaxResponse> {
-    return ajax(this.request(x));
-  }
-  protected operate(rsp: Observable<DataFlowNode>): Observable<DataFlowNode> {
-    return rsp;
-  }
-  protected abstract generateModule(
-    current: DataFlowNode,
-    previous: DataFlowNode
-  ): DataFlowNode;
+	protected abstract prerequest(): Observable<DataFlowNode>;
+	protected abstract request(x: DataFlowNode): AjaxRequest;
+	protected _request(x: DataFlowNode): Observable<AjaxResponse> {
+		return ajax(this.request(x));
+	}
+	protected operate(rsp: Observable<DataFlowNode>): Observable<DataFlowNode> {
+		return rsp;
+	}
+	protected abstract generateModule(current: DataFlowNode, previous: DataFlowNode): DataFlowNode;
 
-  private interalData$: Observable<DataFlowNode>;
-  private data$: Observable<DataFlowNode>;
-  private static cacheData: { [index: string]: DataFlowNode } = {};
-  private moduleRst: DataFlowNode;
+	private interalData$: Observable<DataFlowNode>;
+	private data$: Observable<DataFlowNode>;
+	private static cacheData: { [index: string]: DataFlowNode } = {};
+	private moduleRst: DataFlowNode;
 
-  /**
-   * getOutput
-   */
-  public getDataOutput(): Observable<DataFlowNode> {
-    return this.data$;
-  }
+	/**
+	 * getOutput
+	 */
+	public getDataOutput(): Observable<DataFlowNode> {
+		return this.data$;
+	}
 
-  /**
-   * getModelOutput
-   */
-  public getModuleOutput(): Observable<DataFlowNode> {
-    return this.interalData$.pipe(
-      startWith(this.moduleRst),
-      distinctUntilChanged()
-    );
-  }
+	/**
+	 * getModelOutput
+	 */
+	public getModuleOutput(): Observable<DataFlowNode> {
+		return this.interalData$.pipe(startWith(this.moduleRst), distinctUntilChanged());
+	}
 
-  /**
-   * deploy
-   */
-  public deploy() {
-    const prerequest$ = this.prerequest();
-    // request
-    if (!this.cachePath)
-      this.cachePath = JSON.stringify([this.cmd, this.params]);
-    const request$ = prerequest$.pipe(
-      switchMap(
-        ([data, preErrors]): Observable<DataFlowNode> => {
-          if (preErrors.length !== 0) return prerequest$; // todo: test work around? or replase as of([null, preErrors])
-          return iif(
-            () =>
-              this.cacheSupport &&
-              Generic.cacheData.hasOwnProperty(this.cachePath),
-            of(Generic.cacheData[this.cachePath]),
-            this._request([data, preErrors]).pipe(
-              map((rsp): DataFlowNode => [rsp, []]),
-              catchError((err): DataFlowNode => [{}, [err]]),
-              tap((x: DataFlowNode) => {
-                if(this.cacheSupport)
-                  Generic.cacheData[this.cachePath] = x;
-              })
-            )
-          );
-        }
-      )
-    );
-    // operate
-    const operate$ = this.operate(request$);
-    const interalData$ = operate$.pipe(
-      withLatestFrom(prerequest$),
-      map(([cur, pre]) => this.generateModule(cur, pre)),
-      tap((x) => (this.moduleRst = x))
-    );
-    this.data$ = operate$.pipe(
-      withLatestFrom(interalData$),
-      map(([x, y]) => x)
-    );
-  }
+	/**
+	 * deploy
+	 */
+	public deploy() {
+		const prerequest$ = this.prerequest();
+		// request
+		if (!this.cachePath) this.cachePath = JSON.stringify([this.cmd, this.params]);
+		const request$ = prerequest$.pipe(
+			switchMap(
+				([data, preErrors]): Observable<DataFlowNode> => {
+					if (preErrors.length !== 0) return prerequest$; // todo: test work around? or replase as of([null, preErrors])
+					return iif(
+						() => this.cacheSupport && Generic.cacheData.hasOwnProperty(this.cachePath),
+						of(Generic.cacheData[this.cachePath]),
+						this._request([data, preErrors]).pipe(
+							map((rsp): DataFlowNode => [rsp, []]),
+							catchError((err): DataFlowNode => [{}, [err]]),
+							tap((x: DataFlowNode) => {
+								if (this.cacheSupport) Generic.cacheData[this.cachePath] = x;
+							})
+						)
+					);
+				}
+			)
+		);
+		// operate
+		const operate$ = this.operate(request$);
+		const interalData$ = operate$.pipe(
+			withLatestFrom(prerequest$),
+			map(([cur, pre]) => this.generateModule(cur, pre)),
+			tap((x) => (this.moduleRst = x))
+		);
+		this.data$ = operate$.pipe(
+			withLatestFrom(interalData$),
+			map(([x, y]) => x)
+		);
+	}
 }
