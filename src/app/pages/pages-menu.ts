4652c748685854c5424a5c19ead80b494ce8d2cf
ElonH <elonhhuang@gmail.com>
2020-05-14 23:33:09 +0800

    feat(pages): adding menu 'Server Setting' and 'Apperence Setting'

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,4 +1,5 @@
 import { NbMenuItem } from '@nebular/theme';
+import { group } from '@angular/animations';
 
 export const MENU_ITEMS: NbMenuItem[] = [
 	{
@@ -37,5 +38,17 @@ export const MENU_ITEMS: NbMenuItem[] = [
 		title: 'Jobs Manager',
 		icon: 'briefcase-outline',
 		link: 'jobs',
+	},
+	{
+		title: 'Settings',
+		group: true,
+	},
+	{
+		title: 'Server Setting',
+		icon: 'settings-2-outline', // todo: ng g module pages/settings/server --module app --route settings/server
+	},
+	{
+		title: 'Appearance Setting',
+		icon: 'browser-outline', // todo: ng g module pages/settings/rng --module app --route settings/rng
 	},
 ];
