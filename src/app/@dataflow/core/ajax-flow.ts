8dd95689acb1c446877b547dca5e9a46174148b9
ElonH <elonhhuang@gmail.com>
2020-05-17 18:29:52 +0800

    refactor(ajax-flow): reconstruct AjaxFlow to AjaxFlowInteralNode

--- src/app/@dataflow/core/ajax-flow.ts
+++ src/app/@dataflow/core/ajax-flow.ts
@@ -1,11 +1,11 @@
 import { CacheFlow } from './cache-flow';
-import {  FlowInNode, FlowOutNode, CombErr } from './bare-flow';
+import { FlowInNode, FlowOutNode, CombErr } from './bare-flow';
 import { Observable, of } from 'rxjs';
 import { ajax, AjaxRequest, AjaxResponse } from 'rxjs/ajax';
 import { catchError, map } from 'rxjs/operators';
 import { FlowSupNode } from './superset-flow';
 
-export type AjaxFlowNode = [AjaxResponse | object, Error[]];
+export type AjaxFlowInteralNode = [{ ajaxRsp: AjaxResponse }, Error[]];
 
 export abstract class AjaxFlow<
 	Tin extends FlowInNode,
@@ -16,11 +16,11 @@ export abstract class AjaxFlow<
 	// protected cachePath: string;
 
 	protected abstract requestAjax(pre: CombErr<Tin>): AjaxRequest;
-	protected abstract reconstructAjaxResult(x: AjaxFlowNode): CombErr<Tout>;
+	protected abstract reconstructAjaxResult(x: AjaxFlowInteralNode): CombErr<Tout>;
 	protected requestCache(pre: CombErr<Tin>): Observable<CombErr<Tout>> {
 		return ajax(this.requestAjax(pre)).pipe(
-			map((x) => [x, []] as AjaxFlowNode),
-			catchError((err): Observable<AjaxFlowNode> => of([{}, [err]])),
+			map((x) => [{ajaxRsp: x}, []] as AjaxFlowInteralNode),
+			catchError((err): Observable<AjaxFlowInteralNode> => of([{}, [err]] as AjaxFlowInteralNode)),
 			map((x) => this.reconstructAjaxResult(x))
 		);
 	}
