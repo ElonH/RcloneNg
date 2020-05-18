f370c577af7026d36bbc2b835079817a53299dfa
ElonH <elonhhuang@gmail.com>
2020-05-18 23:47:48 +0800

    feat(dashboard): implement basic framework

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  template: `
-    <p>
-      dashboard works!
-    </p>
-  `,
-  styles: [
-  ]
+	selector: 'app-dashboard',
+	template: `
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<nb-card><nb-card-body>tutorial</nb-card-body></nb-card>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 col-md-6 col-xl-6">
+					<nb-card><nb-card-body>speed</nb-card-body></nb-card>
+				</div>
+				<div class="col-sm-12 col-md-6 col-xl-6">
+					<nb-card><nb-card-body>core/stats</nb-card-body></nb-card>
+				</div>
+				<div class="col-sm-12 col-md-6 col-xl-4">
+					<nb-card><nb-card-body>core/memstats</nb-card-body></nb-card>
+				</div>
+				<div class="col-sm-12 col-md-6 col-xl-4">
+					<nb-card><nb-card-body>cache/stats</nb-card-body></nb-card>
+				</div>
+			</div>
+		</div>
+	`,
+	styles: [],
 })
 export class DashboardComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { NbCardModule } from '@nebular/theme';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+	declarations: [DashboardComponent],
+	imports: [CommonModule, DashboardRoutingModule, NbCardModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
