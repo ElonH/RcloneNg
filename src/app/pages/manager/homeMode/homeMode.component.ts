639725d48acbf0bf74b73d3a0fb64d066083cc16
ElonH <elonhhuang@gmail.com>
2020-05-21 17:52:24 +0800

    feat: setup connect between homeMode and fileMode

--- src/app/pages/manager/homeMode/homeMode.component.ts
+++ src/app/pages/manager/homeMode/homeMode.component.ts
@@ -2,24 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../users.service';
 import { ListRemotesFlow } from 'src/app/@dataflow/rclone';
 import { Subject } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'manager-homeMode',
 	template: `
 		<div class="row justify-content-start">
 			<div class="cloud col-xl-4 col-lg-6 col-md-12" *ngFor="let remote of remotes">
-				<home-view-remote [easyMode]="true" [title]="remote"> </home-view-remote>
+				<home-view-remote [easyMode]="true" [title]="remote" (click)="click(remote)">
+				</home-view-remote>
 			</div>
 		</div>
 	`,
 	styleUrls: ['./homeMode.component.scss'],
 })
 export class HomeModeComponent implements OnInit {
-	constructor(private usersService: UsersService) {}
+	constructor(
+		private usersService: UsersService,
+		private route: ActivatedRoute,
+		private router: Router
+	) {}
 
 	remotesFlow$: ListRemotesFlow;
 	remotesTrigger = new Subject<number>();
 	remotes: string[] = [];
+
+	click(remote: string) {
+		this.router.navigate(['.'], { relativeTo: this.route, queryParams: { remote: remote } });
+	}
 
 	ngOnInit() {
 		const outer = this;
