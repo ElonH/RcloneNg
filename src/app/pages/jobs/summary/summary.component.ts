23fecaf30e6e38d5977c2c28368190b23e2e5d0c
ElonH <elonhhuang@gmail.com>
2020-05-26 22:03:45 +0800

    feat(summary): connect to stats flow

--- src/app/pages/jobs/summary/summary.component.ts
+++ src/app/pages/jobs/summary/summary.component.ts
@@ -5,8 +5,10 @@ import { CoreStatsFlow, CoreStatsFlowOutItemNode } from 'src/app/@dataflow/rclon
 	selector: 'jobs-summary',
 	template: `
 		<dl>
-      <dt>Name</dt>
-      <dd>ElonH</dd>
+			<ng-container *ngFor="let item of keys">
+				<dt>{{ item.title }}</dt>
+				<dd>{{ isDefine(values[item.key]) ? values[item.key] : 'Unknow' }}</dd>
+			</ng-container>
 		</dl>
 	`,
 	styles: [
@@ -29,9 +31,32 @@ export class SummaryComponent implements OnInit {
 	@Input()
 	stats$: CoreStatsFlow;
 
+	keys: { key: string; title?: string }[] = [
+		{ key: 'bytes', title: 'Bytes' },
+		{ key: 'speed', title: 'Speed' },
+		{ key: 'transferring-count', title: 'Transferring' },
+		{ key: 'transfers', title: 'Transferred' },
+		{ key: 'checks', title: 'Checks' },
+		{ key: 'deletes', title: 'Delete' },
+		{ key: 'elapsedTime', title: 'Elapsed Time' },
+		{ key: 'errors', title: 'Errors' },
+		{ key: 'fatalError', title: 'FatalError' },
+		{ key: 'retryError', title: 'RetryError' },
+	];
+	values: CoreStatsFlowOutItemNode = {} as any;
 
 	constructor() {}
+	isDefine(val: any) {
+		return typeof val !== 'undefined';
+	}
 
 	ngOnInit() {
+		this.stats$.getOutput().subscribe(([x, err]) => {
+			if (err.length !== 0) return;
+			this.values = JSON.parse(JSON.stringify(x['core-stats']));
+			this.values['transferring-count'] = this.values.transferring
+				? this.values.transferring.length
+				: 0;
+		});
 	}
 }
