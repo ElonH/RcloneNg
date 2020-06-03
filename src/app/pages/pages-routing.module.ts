8ea21eed335b19ce7d5d97bbe23237e495df90fe
ElonH <elonhhuang@gmail.com>
2020-06-03 20:47:17 +0800

    feat: fetch all module in pre-load stage

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardModule } from './dashboard/dashboard.module';
+import { JobsModule } from './jobs/jobs.module';
+import { ManagerModule } from './manager/manager.module';
 import { PagesComponent } from './pages.component';
+import { UserModule } from './user/user.module';
 
 const routes: Routes = [
 	{
@@ -16,15 +19,18 @@ const routes: Routes = [
 			},
 			{
 				path: 'user',
-				loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+				// loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+				loadChildren: () => UserModule,
 			},
 			{
 				path: 'manager',
-				loadChildren: () => import('./manager/manager.module').then(m => m.ManagerModule),
+				// loadChildren: () => import('./manager/manager.module').then(m => m.ManagerModule),
+				loadChildren: () => ManagerModule,
 			},
 			{
 				path: 'jobs',
-				loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule),
+				// loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule),
+				loadChildren: () => JobsModule,
 			},
 			{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 			{ path: '**', redirectTo: 'dashboard' },
